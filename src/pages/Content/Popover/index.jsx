bffb4c844b43719bc687313cd08275247c5c454c
renhongxu123 <renhongxu123@gmail.com>
2021-07-19 17:19:48 +0800

    content页面注入popover

---
+++ src/pages/Content/Popover/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Popover.css';
+
+const Popup = () => {
+  return (
+    <div className="Popover">
+      <header className="Popover-header">
+        <p>Popover</p>
+      </header>
+      <div>
+        popover content
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
